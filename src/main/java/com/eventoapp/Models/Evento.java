37e01fcd3a786d30d14c9d50e57e9708ad753b1e
Gustavo Alves da Silva <gustavo.silva74@etec.sp.gov.br>
2018-03-03 22:16:47 -0300

    primeiro commit

---
+++ src/main/java/com/eventoapp/Models/Evento.java
@@ -0,0 +1,63 @@
+package com.eventoapp.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Evento implements Serializable {
+
+
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
